3f0f39881f52f9333f2069e208d6c56ab6bf168e
George  Pianka <gpianka1@gmail.com>
2018-03-31 09:17:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,12 @@
+#Print the Board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+#Convert Input to Index
+def input_to_index(input)
+  index = input.to_i - 1 
 end
