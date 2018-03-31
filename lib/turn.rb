9ea04945faeaa708ed658ff475d2ef565d37e010
George  Pianka <gpianka1@gmail.com>
2018-03-31 09:30:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,17 @@ end
 #Return Boolean for valid_move
 def valid_move?(board, index)
   if index.between(0,8) && !position_taken?(board, index)
-    return true 
-  else false 
-  
+    true 
+  else
+    false 
+  end
+end
+# Return Boolean for position_taken
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil 
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true 
+  end
+end
+#
